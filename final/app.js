e7d0a62eca6ed3e337f6c8df5eb48173e3c2b392
LOGANxDUARTE <78757189+LOGANxDUARTE@users.noreply.github.com>
2021-04-04 12:00:29 -0700

    Submitted Project

--- final/app.js
+++ final/app.js
@@ -1,5 +1,13 @@
 const BASE_URL = 'https://jsonplace-univclone.herokuapp.com';
 
+function fetchUserPosts(userId) {
+  return fetchData(`${ BASE_URL }/users/${ userId }/posts?_expand=user`);
+}
+
+function fetchPostComments(postId) {
+  return fetchData(`${ BASE_URL }/posts/${ postId }/comments`);
+}
+
 function fetchUsers() {
     return fetch(`${ BASE_URL }/users`)
     .then(function (response) {
@@ -41,28 +49,22 @@ function renderUserList(userList) {
 }
 
 $('#user-list').on('click', '.user-card .load-posts', function () {
-    $(this).closest()
+    let element = $(this).closest(".user-card").data("user")
     // render posts for this user
-    console.log(this)
+    console.log(element)
   });
   
   $('#user-list').on('click', '.user-card .load-albums', function () {
-    $(this).closest()
-    // render albums for this user
-    console.log(this)
+    let element = $(this).closest(".user-card").data("user");
+    fetchUserAlbumList(element.id).then(renderAlbumList);
   });
+  
 
   // MODULE 2
 
   /* get an album list, or an array of albums */
 function fetchUserAlbumList(userId) {
-  return fetch(`${ BASE_URL }/users/${ userId }/albums`)  
-  .then(function (response) {
-    return response.json()
-  })
-  .catch(function (error) {
-    console.log(error)  
-  })
+  return fetchData(`${ BASE_URL }/users/${ userId }/albums?_expand=user&_embed=photos`)
 }
 
 fetchUserAlbumList(1).then(function (albumList) {
@@ -70,26 +72,136 @@ fetchUserAlbumList(1).then(function (albumList) {
 });
 
 function fetchData(url) {
-  
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
-// /* render a single album */
-// function renderAlbum(album) {
+/* render a single album */
+function renderAlbum(album) {
+  const albumElement = $(`
+  <div class="album-card">
+  <header>
+    <h3>${album.title}, by ${ album.user.username }</h3>
+  </header>
+  <section class="photo-list">
+  <div class="photo-card"></div>
+    <div class="photo-card"></div>
+    <div class="photo-card"></div>
+  </section>
+</div>`)
+    const photoListElement = albumElement.find(".photo-list")
 
-// }
+    album.photos.forEach(function (photo) {
+      const photoElement = renderPhoto(photo)
+      photoListElement.append(photoElement)
+    })
+    return albumElement;
+}
 
-// /* render a single photo */
-// function renderPhoto(photo) {
+/* render a single photo */
+function renderPhoto(photo) {
+    return $(`<div class="photo-card">
+    <a href="${photo.url}" target="_blank">
+      <img src="${photo.thumbnailUrl}">
+      <figure>${photo.title}</figure>
+    </a>
+  </div>`)
 
-// }
+}
 
-// /* render an array of albums */
-// function renderAlbumList(albumList) {
+/* render an array of albums */
+function renderAlbumList(albumList) {
+  $('#app section.active').removeClass('active')
+  $('#album-list').empty().addClass('active')
 
-// }
+albumList.forEach(function (album) {
+    const listElement = renderAlbum(album)
+    $('#album-list').append(listElement)
+  })
+}
+
+// fetchUserAlbumList(1).then(renderAlbumList);
+
+// MODULE 3
+
+function setCommentsOnPost(post) {
+  // if we already have comments, don't fetch them again
+  if (post.comments) {
+    return Promise.reject(null);
+  }
+  // fetch, upgrade the post object, then return it
+  return fetchPostComments(post.id).then(function (comments) {
+    post.comments = comments;
+    return post;
+  });
+}
+function renderPost(post) {
+  return $(`<div class="post-card">
+    <header>
+      <h3>${ post.title }</h3>
+      <h3>--- ${ post.user.username }</h3>
+    </header>
+    <p>${ post.body }</p>
+    <footer>
+      <div class="comment-list"></div>
+      <a href="#" class="toggle-comments">(<span class="verb">show</span> comments)</a>
+    </footer>
+  </div>`).data('post', post)
+}
+
+function renderPostList(postList) {
+  $('#app section.active').removeClass('active');
+  const postListElement = $('#post-list');
+  postListElement.empty().addClass('active');
+  postList.forEach(function (post) {
+    postListElement.append( renderPost(post) );
+  });
+}
+
+function toggleComments(postCardElement) {
+  const footerElement = postCardElement.find('footer');
+  if (footerElement.hasClass('comments-open')) {
+    footerElement.removeClass('comments-open');
+    footerElement.find('.verb').text('show');
+  } else {
+    footerElement.addClass('comments-open');
+    footerElement.find('.verb').text('hide');
+  }
+}
+$('#user-list').on('click', '.user-card .load-posts', function () {
+  const user = $(this).closest('.user-card').data('user');
+  fetchUserPosts(user.id)
+    .then(renderPostList);
+});
+
+$('#post-list').on('click', '.post-card .toggle-comments', function () {
+  const postCardElement = $(this).closest('.post-card');
+  const post = postCardElement.data('post');
+  const commentListElement = postCardElement.find('.comment-list');
+  setCommentsOnPost(post)
+    .then(function (post) {
+      commentListElement.empty();
+      post.comments.forEach(function (comment) {
+        commentListElement.prepend($(`
+          <h3>${ comment.body } --- ${ comment.email }</h3>
+        `));
+      });
+      toggleComments(postCardElement);
+    })
+    .catch(function () {
+      toggleComments(postCardElement);
+    });
+});
+
+// CALL THE PAGE
 
 function bootstrap() {
   fetchUsers().then(renderUserList);
 }
   
-  bootstrap();
+bootstrap();
